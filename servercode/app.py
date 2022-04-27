7b23367a7506981cf9ec81a4a3d6097319b0445b
flaviohpo <flaviohpo@yahoo.com>
2022-04-26 23:56:23 -0300

    inciando refatoracao

--- servercode/app.py
+++ servercode/app.py
@@ -1,28 +1,61 @@
 # import the Flask class from the flask module
 from flask import Flask, render_template, request, redirect, Response
 import yaml
-
-with open("parameters.yaml", 'r') as stream:
-    parameters = yaml.load(stream, Loader=yaml.SafeLoader)
+import os.path
 
 class time:
     def __init__(self, hora=0, min=0, seg=0):
-        self.hora = hora
-        self.min = min
-        self.seg = seg
+        self.__hora = hora
+        self.__min = min
+        self.__seg = seg
     def from_string(self, text):
         '''Sets the values passing a string hh:mm:ss as text'''
-        self.hora = int(text[0:2])
-        self.min = int(text[3:5])
-        self.seg = int(text[6:8])
+        self.__hora = int(text[0:2])
+        self.__min = int(text[3:5])
+        self.__seg = int(text[6:8])
     def __str__(self):
-        return f'{self.hora:02d}:{self.min:02d}:{self.seg:02d}'
+        return f'{self.__hora:02d}:{self.__min:02d}:{self.__seg:02d}'
+    @property
+    def hora(self):
+        return self.__hora
+    @property
+    def min(self):
+        return self.__min
+    @property
+    def seg(self):
+        return self.__seg
 
-horario_inicio = time()
-horario_fim = time()
+class horario_alimentacao:
 
-horario_inicio.from_string(parameters['hora_inicio'])
-horario_fim.from_string(parameters['hora_fim'])
+    def __init__(self, time_inicio=time(), duracao=10):
+        self.__time_inicio=time_inicio
+        self.__duracao=duracao
+
+    @property
+    def time_inicio(self):
+        return self.__time_inicio
+
+    @property
+    def duracao(self):
+        return self.__duracao
+
+    def to_dict(self):
+        return {"Inicio": self.__time_inicio, "Duracao": self.__duracao}
+
+
+horario = horario_alimentacao(time(10,10,10),10)
+
+if os.path.isfile('parameters.yaml'):
+    print('O arquivo existe')
+else:
+    print('O arquivo nao existe')
+    with open('parameters.yaml', 'w') as outfile:
+        yaml.dump({'horario1': horario}, outfile, default_flow_style=False)
+
+
+
+#with open("parameters.yaml", 'r') as stream:
+#    parameters = yaml.load(stream, Loader=yaml.SafeLoader)
 
 # create the application object
 app = Flask(__name__)
@@ -30,29 +63,4 @@ app = Flask(__name__)
 # use decorators to link the function to a url
 @app.route('/')
 def home():
-    return render_template('home.html', text1=str(horario_inicio), text2=str(horario_fim))  # render a template
-
-@app.route('/set_value', methods=['POST',])
-def set_value():
-    '''Utilizado pelo browser'''
-    texto_horario_inicio = request.form['horario_inicio']
-    texto_horario_fim = request.form['horario_fim']
-    horario_inicio.from_string(texto_horario_inicio)
-    horario_fim.from_string(texto_horario_fim)
-    parameters['hora_inicio'] = str(horario_inicio)
-    parameters['hora_fim'] = str(horario_fim)
-    with open('parameters.yaml', 'w') as outfile:
-        yaml.dump(parameters, outfile, default_flow_style=False)
-    return ('', 204)
-
-@app.route('/get_value')
-def get_value():
-    '''Utilizado pelo hardware'''
-    resp = Response("horarios")
-    resp.headers['horario_inicio'] = str(horario_inicio)
-    resp.headers['horario_fim'] = str(horario_fim)
-    return resp
-
-# start the server with the 'run()' method
-if __name__ == '__main__':
-    app.run(debug=True)
+    return "CARAI CACHOERA"
---
+++ servercode/app_old.py
@@ -0,0 +1,67 @@
+# import the Flask class from the flask module
+from flask import Flask, render_template, request, redirect, Response
+import yaml
+
+with open("parameters.yaml", 'r') as stream:
+    parameters = yaml.load(stream, Loader=yaml.SafeLoader)
+
+class time:
+    def __init__(self, hora=0, min=0, seg=0):
+        self.__hora = hora
+        self.__min = min
+        self.__seg = seg
+    def from_string(self, text):
+        '''Sets the values passing a string hh:mm:ss as text'''
+        self.__hora = int(text[0:2])
+        self.__min = int(text[3:5])
+        self.__seg = int(text[6:8])
+    def __str__(self):
+        return f'{self.__hora:02d}:{self.__min:02d}:{self.__seg:02d}'
+    @property
+    def hora(self):
+        return self.__hora
+    @property
+    def min(self):
+        return self.__min
+    @property
+    def seg(self):
+        return self.__seg
+
+horario_inicio = time()
+horario_fim = time()
+
+horario_inicio.from_string(parameters['hora_inicio'])
+horario_fim.from_string(parameters['hora_fim'])
+
+# create the application object
+app = Flask(__name__)
+
+# use decorators to link the function to a url
+@app.route('/')
+def home():
+    return render_template('home.html', text1=str(horario_inicio), text2=str(horario_fim))  # render a template
+
+@app.route('/set_value', methods=['POST',])
+def set_value():
+    '''Utilizado pelo browser'''
+    texto_horario_inicio = request.form['horario_inicio']
+    texto_horario_fim = request.form['horario_fim']
+    horario_inicio.from_string(texto_horario_inicio)
+    horario_fim.from_string(texto_horario_fim)
+    parameters['hora_inicio'] = str(horario_inicio)
+    parameters['hora_fim'] = str(horario_fim)
+    with open('parameters.yaml', 'w') as outfile:
+        yaml.dump(parameters, outfile, default_flow_style=False)
+    return ('', 204)
+
+@app.route('/get_value')
+def get_value():
+    '''Utilizado pelo hardware'''
+    resp = Response("horarios")
+    resp.headers['horario_inicio'] = str(horario_inicio)
+    resp.headers['horario_fim'] = str(horario_fim)
+    return resp
+
+# start the server with the 'run()' method
+if __name__ == '__main__':
+    app.run(debug=True)
